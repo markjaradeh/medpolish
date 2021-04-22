0b6c4dddee6bb8c0f06fd569b7a2e1950bc94fd7
markjaradeh <77334243+markjaradeh@users.noreply.github.com>
2021-04-22 06:10:46 -0500

    Update and rename account.html to register.php

---
+++ register.php
@@ -0,0 +1,62 @@
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Med Polish Register</title>
+        <link rel="icon" href="/images/favicon.ico"/>
+        <!-- Bootstrap core CSS -->
+        <link href="https://fonts.googleapis.com/css?family=PT+Sans:400,400i,700,700i" rel="stylesheet">  
+        <!-- Custom styles for this template -->
+        <link href="/css/style.css" rel="stylesheet">
+        <link href="/css/bootstrap.min.css" rel="stylesheet">
+    </head>
+    <body class="content">
+        <header>
+            <div class="logo text-center">
+                <h2>Welcome to Medical Polish!</h2>
+            </div>
+        </header>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-offset-4 col-md-4 col-sm-offset-3 col-sm-6 col-xs-offset-3 col-xs-6 width-100">
+                    <h2>Register</h2>
+                    <p> Please fill out the indicated fields to create an account</p>
+                    <form action="#" method="post">
+                        <div class="form-group">
+                            <label>Full Name</label>
+                            <input class="form-control" type="text" name="name" required>
+                        </div>
+                        <div class="form-group">
+                            <label>Email Address</label>
+                            <input class="form-control" type="email" name="email" required>
+                        </div>
+                        <div class="form-group">
+                            <label>Password</label>
+                            <input class="form-control" type="password" name="password" required>
+                        </div>
+                        <div class="form-group">
+                            <label>Confirm Password</label>
+                            <input class="form-control" type="password" name="confirm_password" required>
+                        </div>
+                        <div class="form-group">
+                            <input class="btn btn-primary" value="Submit" type="submit" name="submit" required>
+                        </div>
+                        <p>Already have an account? <a href="login.php">Login here</a></p>
+                    </form>
+                </div>
+            </div>
+            <div class="row">
+                <div class="copyright-box">
+                    <div class="copyright">
+                        <!--Do not remove Backlink from footer of the template. To remove it you can purchase the Backlink !-->
+                        &copy; 2021 All right reserved. <a href="http://www.medpolish.com/" target="_blank"><strong>MedPolish.</strong></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+
+</html>
